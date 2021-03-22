8eea22fb2ed2d6284a467faae6eb24ceebe62237
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-03-22 22:33:31 +0700

    video 2 - Typography

---
+++ src/pages/Create.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+export default function Create() {
+  return (
+    <div>
+      <Typography variant='h6' component='h2' gutterBottom color='textPrimary'>
+        Create a New Note
+      </Typography>
+      <Typography color='secondary' noWrap>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem repudiandae dolor at perferendis a quo obcaecati
+        ipsam aperiam dicta officia labore, reiciendis laudantium quibusdam possimus assumenda eligendi voluptatum earum
+        quasi?
+      </Typography>
+    </div>
+  );
+}
