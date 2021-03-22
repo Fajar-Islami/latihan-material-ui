8f9aabccd794f28c7b5f88ffb7ed516c99dd29f6
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-03-23 06:53:41 +0700

    video 5 - makeStyle

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -3,15 +3,33 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import KeyboardArrowRightOutlinedIcon from "@material-ui/icons/KeyboardArrowRightOutlined";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: "violet",
+    "&:hover": {
+      backgroundColor: "blue",
+    },
+  },
+  title: {
+    textDecoration: "underline",
+    marginBlockStart: 20,
+  },
+});
 
 export default function Create() {
+  const classes = useStyles();
+
   return (
     <Container>
-      <Typography variant='h6' component='h2' gutterBottom color='textPrimary'>
+      <Typography variant='h6' component='h2' gutterBottom color='textPrimary' className={classes.title}>
         Create a New Note
       </Typography>
 
       <Button
+        className={classes.btn}
         type='submit'
         color='secondary'
         variant='contained'
