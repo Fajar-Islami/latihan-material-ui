12b656f96076811da13c3acfbe47815985b6651f
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-03-22 22:44:02 +0700

    video 3 - button

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,17 +1,29 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
 
 export default function Create() {
   return (
-    <div>
+    <Container>
       <Typography variant='h6' component='h2' gutterBottom color='textPrimary'>
         Create a New Note
       </Typography>
-      <Typography color='secondary' noWrap>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem repudiandae dolor at perferendis a quo obcaecati
-        ipsam aperiam dicta officia labore, reiciendis laudantium quibusdam possimus assumenda eligendi voluptatum earum
-        quasi?
-      </Typography>
-    </div>
+
+      <Button type='submit' color='secondary' variant='contained' onClick={() => console.log("click button")}>
+        Submit
+      </Button>
+
+      {/* <Button type='submit'>Submit</Button>
+      <Button type='submit' color='secondary' variant='outlined'>
+        Submit
+      </Button>
+
+      <ButtonGroup color='secondary' variant='contained'>
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+    </Container>
   );
 }
