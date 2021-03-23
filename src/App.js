4778fe09b91b8bb7d689364afa5a597dbbe1d0ca
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-03-23 07:23:10 +0700

    video 7 - custom theme

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Notes from "./pages/Notes";
 import Create from "./pages/Create";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import { purple } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#fefefe",
+    },
+    secondary: purple,
+  },
+  typography: {
+    fontFamily: "Quicksand",
+    fontWeightLight: 400, // dari impor di index.css
+    fontWeightRegular: 500, // dari impor di index.css
+    fontWeightMedium: 600, // dari impor di index.css
+    fontWeightBold: 700, // dari impor di index.css
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/'>
-          <Notes />
-        </Route>
-        <Route path='/create'>
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Notes />
+          </Route>
+          <Route path='/create'>
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -3,35 +3,17 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import KeyboardArrowRightOutlinedIcon from "@material-ui/icons/KeyboardArrowRightOutlined";
-import { makeStyles } from "@material-ui/core";
-
-const useStyles = makeStyles({
-  btn: {
-    fontSize: 60,
-    backgroundColor: "violet",
-    "&:hover": {
-      backgroundColor: "blue",
-    },
-  },
-  title: {
-    textDecoration: "underline",
-    marginBlockStart: 20,
-  },
-});
 
 export default function Create() {
-  const classes = useStyles();
-
   return (
     <Container>
-      <Typography variant='h6' component='h2' gutterBottom color='textPrimary' className={classes.title}>
+      <Typography variant='h6' component='h2' gutterBottom color='textPrimary'>
         Create a New Note
       </Typography>
 
       <Button
-        className={classes.btn}
         type='submit'
-        color='secondary'
+        color='primary'
         variant='contained'
         onClick={() => console.log("click button")}
         endIcon={<KeyboardArrowRightOutlinedIcon />}
